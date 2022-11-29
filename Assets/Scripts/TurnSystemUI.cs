ef8917e4b99627e371d4b1c8ddac8dedf523a46c
AdityaDheer <ad4961@nyu.edu>
2022-11-29 14:35:45 -0500

    added visual indication for the enemy turn

--- Assets/Scripts/TurnSystemUI.cs
+++ Assets/Scripts/TurnSystemUI.cs
@@ -9,6 +9,7 @@ public class TurnSystemUI : MonoBehaviour
 {
     [SerializeField] private Button endTurnButton;
     [SerializeField] private TextMeshProUGUI turnText;
+    [SerializeField] private GameObject enemyTurnVisualGameObject;
 
     private void Start()
     {
@@ -20,11 +21,15 @@ public class TurnSystemUI : MonoBehaviour
         TurnSystem.Instance.OnTurnChanged += TurnSystem_OnTurnChanged;
 
         UpdateTurnText();
+        UpdateEnemyTurnVisual();
+        UpdateEndTurnButtonVisibility();
     }
 
     private void TurnSystem_OnTurnChanged(object sender, EventArgs e)
     {
         UpdateTurnText();
+        UpdateEnemyTurnVisual();
+        UpdateEndTurnButtonVisibility();
     }
 
     private void UpdateTurnText()
@@ -32,5 +37,16 @@ public class TurnSystemUI : MonoBehaviour
         turnText.text = "TURN " + TurnSystem.Instance.GetTurnNumber();
     }
 
+    // updates the enemey turn visual
+    private void UpdateEnemyTurnVisual()
+    {
+        enemyTurnVisualGameObject.SetActive(!TurnSystem.Instance.IsPlayerTurn());
+    }
+
+    // remove the end turn button if it is the enemies turn
+    private void UpdateEndTurnButtonVisibility()
+    {
+        endTurnButton.gameObject.SetActive(TurnSystem.Instance.IsPlayerTurn());
+    }
 
 }
