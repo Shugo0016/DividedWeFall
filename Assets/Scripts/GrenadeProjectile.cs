cc7eff45ddf6da6915e0945683d4df1d9a4f2e63
AdityaDheer <ad4961@nyu.edu>
2022-12-04 22:05:32 -0500

    added grenade action

---
+++ Assets/Scripts/GrenadeProjectile.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GrenadeProjectile : MonoBehaviour
+{
+    private Vector3 targetPosition;
+
+    private float damageRadius = 4f;
+
+    private Action onGrenadeBehaviourComplete;
+
+    private void Update()
+    {
+        Vector3 moveDir = (targetPosition - transform.position).normalized;
+
+        float moveSpeed = 15f;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        float reachedTargetDistance = 0.2f;
+        if (Vector3.Distance(transform.position, targetPosition) < reachedTargetDistance)
+        {
+            Collider[] colliderArray = Physics.OverlapSphere(targetPosition, damageRadius);
+
+            foreach (Collider collider in colliderArray)
+            {
+                if (collider.TryGetComponent<Unit>(out Unit targetUnit))
+                {
+                    targetUnit.TakeDamage(30);
+                }
+            }
+            Destroy(gameObject);
+
+            onGrenadeBehaviourComplete();
+        }
+    }
+    public void Setup(GridPosition targetGridPosition, Action onGrenadeBehaviourComplete)
+    {
+        this.onGrenadeBehaviourComplete = onGrenadeBehaviourComplete;
+        targetPosition = LevelGrid.Instance.GetWorldPosition(targetGridPosition);
+    }
+}
